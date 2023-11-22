d665972e58bc14a7c8111dfabd9162f6b41a4b04
Ryssia <ryssia.elias@gmail.com>
2023-11-22 09:30:18 -0300

    correção metodos em vermelho

--- src/Service/ProcessaCompra.php
+++ src/Service/ProcessaCompra.php
@@ -6,7 +6,7 @@ use Loja\WebIII\Model\Carrinho;
 use Loja\WebIII\Model\Produto;
 
 class ProcessaCompra {
-    
+
     /** @var Carrinho */
     private $carrinho;
     /** @var int */
@@ -15,40 +15,38 @@ class ProcessaCompra {
     private $totalDaCompra;
     private $menorValor, $maiorValor;
 
-        if($produto->getValor() > $this->maiorValor){
-        $this->maiorValor = $produto->getValor();
-        }
-        else if($produto->getValor() < $this->menorValor){
-        $this->menorValor = $produto->getValor();
-        }
-
     public function __construct() {
         $this->totalDaCompra = 0;
+        $this->menorValor = PHP_FLOAT_MAX; // Inicializa com o maior valor possível
+        $this->maiorValor = 0; // Inicializa com o menor valor possível
     }
 
+    public function processaProduto(Produto $produto) {
+        if ($produto->getValor() > $this->maiorValor) {
+            $this->maiorValor = $produto->getValor();
+        } elseif ($produto->getValor() < $this->menorValor) {
+            $this->menorValor = $produto->getValor();
+        }
+    }
 
     public function getProdutoDeMaiorValor(): float {
-    return $this->maiorValor;
-    }
-    public function getProdutoDeMenorValor(): float
-    {
-    return $this->menorValor;
+        return $this->maiorValor;
     }
 
-    public function finalizaCompra(Carrinho $carrinho)
-    {
+    public function getProdutoDeMenorValor(): float {
+        return $this->menorValor;
+    }
+
+    public function finalizaCompra(Carrinho $carrinho) {
         $this->carrinho = $carrinho;
         $produtos = $this->carrinho->getProdutos();
-        foreach($produtos as $produto){
-           $this->totalDaCompra = $this->totalDaCompra + $produto->getValor();
+        foreach ($produtos as $produto) {
+            $this->totalDaCompra += $produto->getValor();
+            $this->processaProduto($produto);
         }
-        
     }
 
-    public function getTotalDaCompra(): float
-    {
+    public function getTotalDaCompra(): float {
         return $this->totalDaCompra;
     }
-
-    
 }
